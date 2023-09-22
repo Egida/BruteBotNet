12a153ffa4fa4dafc764669f9fcedc005dc7474f
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-22 05:35:35 +0400

    Update Run-Server.py

--- Run-Server.py
+++ Run-Server.py
@@ -67,13 +67,10 @@ os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf SERVER! | lolca
 while len(ports) < 10:
     ports.append(0)
 
-# Specify the number of ports to use (up to 10)
 num_ports_to_use = min(len(ports), 10)
 
-# Extract the first 10 ports from the 'ports' list
 ports_to_use = ports[:num_ports_to_use]
 
-# Replace 'Port1', 'Port2', etc. with the actual ports from the list
 command = f"php -S 127.0.0.1:{Loc} & gnome-terminal -- python3 Networkreports.py & gnome-terminal -- python3 Map.py & gnome-terminal -- python3 Del.py & gnome-terminal -- python3 TargetsData.py & gnome-terminal -- python3 WebStatus.py"
 for i, port in enumerate(ports_to_use, start=1):
     command += f" & gnome-terminal -- ./bore local {Loc} --to bore.pub -p {port}"
