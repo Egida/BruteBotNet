7cd0d5d80cece7fc189dded36b397d552d73c6f9
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-23 07:08:17 +0400

    Update WebStatus.py

--- WebStatus.py
+++ WebStatus.py
@@ -3,7 +3,14 @@ import os
 import time
 from termcolor import colored
 
-def check_website_status(ports):
+def safe_input(prompt):
+    try:
+        return input(prompt)
+    except UnicodeDecodeError:
+        print("Invalid input. Please enter valid characters.")
+        return safe_input(prompt)
+
+def check_bore_status(ports):
     while True:
         os.system("clear")
         os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf WebStatus | lolcat")
@@ -34,13 +41,73 @@ def check_website_status(ports):
         time.sleep(5)  
         os.system("clear")
 
-def get_ports():
-    if os.path.exists("Ports.io"):
-        with open("Ports.io", "r") as file:
+def check_serveo_status(ports):
+    while True:
+        os.system("clear")
+        os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf WebStatus | lolcat")
+        websites = [f"https://serveo.net:{port}/WebStatus" for port in ports]
+
+        for website in websites:
+            try:
+                response = requests.get(website)
+                response.raise_for_status() 
+                status = colored("[+] Connected", 'green')
+                response_time = response.elapsed.total_seconds()
+                status_code = response.status_code
+            except requests.exceptions.RequestException as e:
+                status = colored("[-] Not Connected", 'red')
+                response_time = None
+                status_code = None
+                error_message = str(e)
+
+            print("=" * 40)
+            print(f"Website: {website}")
+            print(f'Status: {status}')
+            if status == colored("[+] Connected", 'green'):
+                print(f"Response Time: {response_time} seconds")
+            else:
+                print(f"Error Check WebSite!")
+            print("=" * 40)
+
+        time.sleep(5)  
+        os.system("clear")
+
+def check_serveo_domain_status(domain):
+    while True:
+        os.system("clear")
+        os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf WebStatus | lolcat")
+        website = f"https://{domain}.serveo.net/WebStatus"
+
+        try:
+            response = requests.get(website)
+            response.raise_for_status() 
+            status = colored("[+] Connected", 'green')
+            response_time = response.elapsed.total_seconds()
+            status_code = response.status_code
+        except requests.exceptions.RequestException as e:
+            status = colored("[-] Not Connected", 'red')
+            response_time = None
+            status_code = None
+            error_message = str(e)
+
+        print("=" * 40)
+        print(f"Website: {website}")
+        print(f'Status: {status}')
+        if status == colored("[+] Connected", 'green'):
+            print(f"Response Time: {response_time} seconds")
+        else:
+            print(f"Error Check WebSite!")
+        print("=" * 40)
+
+        time.sleep(5)  
+        os.system("clear")
+
+def get_ports_from_file(file_path):
+    try:
+        with open(file_path, "r") as file:
             lines = file.readlines()
 
         ports = []
-
         for line in lines:
             parts = line.strip().split("=")
             if len(parts) == 2:
@@ -49,19 +116,28 @@ def get_ports():
                     ports.append(port)
                 except ValueError:
                     print(f"Invalid port value: {parts[1].strip()}")
-    else:
-        print("No 'Ports.io' file found. Please enter 10 ports manually.")
-        ports = []
-        for i in range(1, 11):
-            while True:
-                try:
-                    port = int(input(f"Your Server Port {i} : "))
-                    ports.append(port)
-                    break
-                except ValueError:
-                    print("Invalid input. Please enter a valid integer.")
-    return ports
 
-if __name__ == "__main__":
-    ports = get_ports()
-    check_website_status(ports)
+        return ports
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return []
+
+print("Choose your connectivity service provider:")
+print("1 - BORE")
+print("2 - SERVEO")
+print("3 - SERVEO Domain")
+
+while True:
+    SERVICE = safe_input("Choose (1, 2, or 3): ")
+    
+    if SERVICE == "1":
+        ports = get_ports_from_file("Ports.io")
+        check_bore_status(ports)
+    elif SERVICE == "2":
+        ports = get_ports_from_file("Ports.io")
+        check_serveo_status(ports)
+    elif SERVICE == "3":
+        DOMAIN = input("Enter Your Domain: ")
+        check_serveo_domain_status(DOMAIN)
+    else:
+        print("Invalid choice. Please choose 1, 2, or 3.")
