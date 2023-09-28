34213e9a83f68fa9188f791ffe9268a5ec163963
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-29 00:25:54 +0400

    Update DirectShell.py

--- DirectShell.py
+++ DirectShell.py
@@ -1,11 +1,9 @@
 import requests
 import os
 import time
-from termcolor import *
+from termcolor import colored
+
 # Function to read the port from the LPort.io file
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf PowerShell | lolcat")
-print("=" * 40)
 def read_port_from_file():
     if os.path.exists('LPort.io'):
         with open('LPort.io', 'r') as file:
@@ -21,8 +19,6 @@ def send_data():
     if port is None:
         return
 
-    if os.path.exists('Data.io'):
-        os.remove('Data.io')
     while True:
         data = input("PowerShell >>> ")
         print("")
@@ -31,11 +27,21 @@ def send_data():
             break
 
         response = requests.post(f'http://localhost:{port}/Control.php', data={'data': data})
-        X = colored(response.text, 'green')
-        print(X)
+
+        if response.status_code == 200:
+            response_data = response.json()  # Assuming the PHP script returns JSON data
+            command_output = response_data.get('command_output', '')
+            command_error = response_data.get('command_error', '')
+
+            if command_output:
+                print(colored(command_output, 'green'))
+
+            if command_error:
+                print(colored(command_error, 'red'))
+        else:
+            print("Error sending data to the server.")
+
         time.sleep(10)
-        if os.path.exists('Data.io'):
-            os.remove('Data.io')
 
 if __name__ == '__main__':
     send_data()
