c409c9f83a2fcd0d9ede84c39cbbc494f01378ca
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-28 15:21:09 +0400

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,18 +1,21 @@
 import os
 import re
 import time
-import folium
-from geopy.geocoders import Nominatim
+import requests
+import plotly.graph_objects as go
+ 
 
-def clear_screen():
-    if os.name == 'nt':  
-     os.system('cls')
-    else:  
-     os.system('clear')
-     os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf IP Map | lolcat")
+os.system("clear")
+os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf IP Map | lolcat")
 
-        
-directory_path = "."
+# Replace with your preferred geocoding service (e.g., GeoIP2, ipinfo.io)
+GEOCODING_SERVICE_URL = "https://ipinfo.io"
+HTML_FILE_NAME = "ip_globe.html"
+UPDATE_INTERVAL_SECONDS = 5  # Update the globe every 5 minutes
+
+def delete_existing_html_file():
+    if os.path.exists(HTML_FILE_NAME):
+        os.remove(HTML_FILE_NAME)
 
 def extract_ip_addresses(file_path):
     ip_addresses = []
@@ -22,44 +25,64 @@ def extract_ip_addresses(file_path):
         ip_addresses.extend(re.findall(ip_pattern, content))
     return ip_addresses
 
-def create_ip_map(ip_list):
-    m = folium.Map(location=[0, 0], zoom_start=2)  
-    geolocator = Nominatim(user_agent="ip-geolocator")
+def get_location(ip):
+    response = requests.get(f"{GEOCODING_SERVICE_URL}/{ip}/json")
+    if response.status_code == 200:
+        data = response.json()
+        if 'loc' in data:
+            lat, lon = data['loc'].split(',')
+            return float(lat), float(lon)
+    return None
 
-    for ip in ip_list:
-        location = get_location(ip, geolocator)
+def create_ip_globe(ip_list):
+    fig = go.Figure()
+
+    # Add a scatter plot of IP addresses
+    for ip_info in ip_list:
+        location = get_location(ip_info)
         if location:
-            folium.Marker(
-                location=location,
-                popup=f"IP: {ip}",
-            ).add_to(m)
+            lat, lon = location
+            fig.add_trace(
+                go.Scattergeo(
+                    lon=[lon],
+                    lat=[lat],
+                    hovertext=[f"IP: {ip_info}"],
+                    mode="markers",
+                    marker=dict(size=8, color="red"),
+                )
+            )
 
-    return m
+    # Customize the layout of the globe
+    fig.update_geos(
+        projection_type="orthographic",
+        showland=True,
+        landcolor="rgb(243, 243, 243)",
+        countrycolor="rgb(204, 204, 204)",
+    )
 
-def get_location(ip, geolocator):
-    try:
-        location = geolocator.geocode(ip)
-        if location:
-            return [location.latitude, location.longitude]
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return None
+    # Set the layout of the figure
+    fig.update_layout(
+        title="IP Addresses on the Globe",
+        geo=dict(
+            showcoastlines=True,
+        ),
+    )
+
+    # Save the figure as an HTML file
+    fig.write_html(HTML_FILE_NAME)
+
+# Delete the existing HTML file (if any)
+delete_existing_html_file()
 
 while True:
     extracted_ip_addresses = []
 
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
-        if os.path.isfile(file_path):
-            extracted_ip_addresses.extend(extract_ip_addresses(file_path))
+    for filename in os.listdir():
+        if os.path.isfile(filename):
+            extracted_ip_addresses.extend(extract_ip_addresses(filename))
 
-    ip_map = create_ip_map(extracted_ip_addresses)
+    create_ip_globe(extracted_ip_addresses)
 
-    os.system("rm -rf ip_location_map.html")
-    os.system("clear")    
-    ip_map.save("ip_location_map.html") 
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf IP Map | lolcat")
-    print
-    print(f"Map saved as 'ip_location_map.html'")
-    print("Loading >>>")
-    time.sleep(1)
+    print()
+    print(f"Globe updated and saved as '{HTML_FILE_NAME}'. Waiting for the next update...")
+    time.sleep(UPDATE_INTERVAL_SECONDS)
