db50616727da54ca27d2950cb078e958f27e872a
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-05 22:54:17 +0400

    Update Metasploit.php

--- Metasploit.php
+++ Metasploit.php
@@ -1,12 +1,17 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Store the received data in a file or database
     $mac = $_POST['MAC'];
     $option = $_POST['Option'];
-    
+    $target = $_POST['Target'];
+
+    // Build a data string with the received values
+    $data = "$mac::$option::$target";
+
     // Store the data in a file (you can use a database as well)
-    $data = "$mac::$option";
     file_put_contents('Meta.io', $data);
+
+    // Respond with a success message
+    echo "Data received and stored successfully.";
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Serve the stored data when requested
     if (file_exists('Meta.io')) {
