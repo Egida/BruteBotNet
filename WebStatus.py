086022c0730f2f9a2e5d9cc94a326fb87a955b0d
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-06 21:01:44 +0400

    Update WebStatus.py

--- WebStatus.py
+++ WebStatus.py
@@ -1,71 +1,41 @@
-import os
 import requests
 import time
 from termcolor import colored
+import os
 
-
-
+# Tor proxy configuration
 proxies = {
-    'http': 'socks5h://127.0.0.1:9150',  # Tor proxy address and port for HTTP
-    'https': 'socks5h://127.0.0.1:9150'  # Tor proxy address and port for HTTPS
+    'http': 'socks5h://127.0.0.1:9151',  # Tor proxy address and port for HTTP
+    'https': 'socks5h://127.0.0.1:9151'  # Tor proxy address and port for HTTPS
 }
 
+# Create a Requests session with proxy configuration
 requests_session = requests.Session()
 requests_session.proxies.update(proxies)
 
+# The URL of the page you want to check
+url = 'http://fxs4h3mj7ei3kb6lpd3souicoe3cmoechccu532szgiulydji7dk7kqd.onion/WebStatus'
 
-possible_paths = [
-    "/var/lib/tor/Domain/hostname",
-    "~/hostname",
-    "~/TorDomainInfo/Domain/hostname",
-]
+while True:
+    response = requests_session.get(url)
 
-def find_tor_host_file():
-    for path in possible_paths:
-        expanded_path = os.path.expanduser(path)
-        if os.path.isfile(expanded_path):
-            return expanded_path
-    return None
-
-def read_tor_domain(file_path):
-    try:
-        with open(file_path, "r") as file:
-            return file.readline().strip()
-    except FileNotFoundError:
-        return None
-
-def check_tor_domain_status(tor_domain):
-    while True:
-        try:
-            tor_address = f"http://{tor_domain}/WebStatus"
-
-            response = requests_session.get(tor_address)
-            response.raise_for_status()
-            status = colored("[+] Connected", 'green')
-            response_time = response.elapsed.total_seconds()
-        except requests.exceptions.RequestException as e:
-            status = colored("[-] Not Connected", 'red')
-            response_time = None
-
-        os.system("clear")
-        os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf WebStatus | lolcat")
-        print("=" * 40)
-        print(f"Tor Domain: {tor_domain}")
-        print(f'Status: {status}')
-        if status == colored("[-] Not Connected", 'red'):
-            print("Error Checking Tor Domain!")
-        elif response_time is not None:
-            print(f"Response Time: {response_time:.5f} seconds")
-        print("=" * 40)
-
-        time.sleep(5)
-
-tor_host_file = find_tor_host_file()
-if tor_host_file:
-    tor_domain = read_tor_domain(tor_host_file)
-    if tor_domain:
-        check_tor_domain_status(tor_domain)
+    if response.status_code == 200:
+        status = colored("[+] Connected", 'green')
     else:
-        print(f"Tor domain not found in {tor_host_file}")
-else:
-    print("Tor host file not found in any of the specified paths.")
+        status = colored("[-] Not Connected", 'red')
+
+    response_time = response.elapsed.total_seconds() if response.elapsed else None
+
+    # Page information and response
+    os.system("clear")
+    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf WebStatus | lolcat")
+    print("=" * 40)
+    print(f"Page URL: {url}")
+    print(f'Status: {status}')
+    if status == colored("[-] Not Connected", 'red'):
+        print("Error: Missing dependencies for SOCKS support.")
+    elif response_time is not None:
+        print(f"Response Time: {response_time:.5f} seconds")
+    print("=" * 40)
+
+    time.sleep(5)  # Wait for some time before performing a new check
