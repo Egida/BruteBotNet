91df9230f570d5023a43410e0a592d7b109506df
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-18 22:44:57 +0400

    Update Setup-Server.sh

--- Setup-Server.sh
+++ Setup-Server.sh
@@ -23,11 +23,6 @@ systemctl start tor
 service tor start
 
 sudo echo "HiddenServiceDir /var/lib/tor/Domain/
-HiddenServicePort 80 127.0.0.1:8080" | sudo tee -a /etc/tor/torrc
-echo -e "SocksPort 127.0.0.1:9151" | sudo tee -a /etc/tor/torrc
-
-
-sudo echo "HiddenServiceDir /var/lib/tor/Domain/
 HiddenServicePort 80 127.0.0.1:8080
 HiddenServicePort 80 127.0.0.1:8070
 HiddenServicePort 80 127.0.0.1:8060" | sudo tee -a /etc/tor/torrc
