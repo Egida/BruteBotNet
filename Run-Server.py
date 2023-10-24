2ccde0882231db6d444729f45df3757776ed8dfe
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-24 22:04:09 +0400

    Update Run-Server.py

--- Run-Server.py
+++ Run-Server.py
@@ -1,61 +1,59 @@
-import time
-from termcolor import colored
-import sys
 import os
+import subprocess
+import time
+import requests
 
-# Check for root privileges
-if os.geteuid() != 0:
-    print("You must run the script as a root user or with sudo privileges.")
-    sys.exit(1)
+# Install necessary packages
+packages = ["python3-pip", "python3-socks", "php", "dbus-x11", "gnome-terminal"]
+subprocess.run(["sudo", "apt", "update"])
+subprocess.run(["sudo", "apt", "install"] + packages + ["-y"])
 
-os.system("sudo systemctl start tor; service tor start")
-time.sleep(0.5)
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Setup. | lolcat")
-time.sleep(0.5)
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Setup.. | lolcat")
-time.sleep(0.5)
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Setup... | lolcat")
-print("=" * 60)
-Loc = "8080"
-print("LocalHost Port 8080")
-print("=" * 60)
-print("")
-print("=" * 60)
-print("TOR will be the main service provider")
-print("=" * 60)
-time.sleep(3)
+# Clone figlet fonts
+os.makedirs("~/.local/share/fonts/figlet-fonts/", exist_ok=True)
+subprocess.run(["git", "clone", "https://github.com/xero/figlet-fonts.git", "~/.local/share/fonts/figlet-fonts/"])
+os.system("cd ~/.local/share/fonts/figlet-fonts/ ; mv 'ANSI Regular.flf' Reg.flf")
+# Install Python packages
+python_packages = ["folium", "geopy", "psutil", "wmi", "Dispatch", "ping3", "termcolor", "plotly", "mss", "opencv-python"]
+subprocess.run(["pip", "install"] + python_packages)
 
-for _ in range(1):
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Running. | lolcat")
-    time.sleep(0.5)
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Running.. | lolcat")
-    time.sleep(0.5)
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Running... | lolcat")
-    time.sleep(0.5)
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Running. | lolcat")
-    time.sleep(0.5)
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Running.. | lolcat")
-    time.sleep(0.5)
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Running... | lolcat")
-    time.sleep(0.5)
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf SERVER! | lolcat")
-os.system("service tor restart")
-time.sleep(10)
+# Install and start Tor
+subprocess.run(["sudo", "apt", "install", "tor", "-y"])
+subprocess.run(["sudo", "systemctl", "start", "tor"])
+subprocess.run(["sudo", "service", "tor", "start"])
 
-command = f"gnome-terminal -- php -S 127.0.0.1:{Loc} & gnome-terminal -- python3 Networkreports.py & gnome-terminal -- python3 Map.py & gnome-terminal -- python3 Del.py & gnome-terminal -- python3 TargetsData.py & gnome-terminal -- python3 WebStatus.py & gnome-terminal -- python3 DirectShell.py & gnome-terminal -- python3 Meta.py"
-os.system(command)
-time.sleep(3)
-print(f"Your Tor Domain : ")
-os.system("cat /var/lib/tor/Domain/hostname")
+# Configure Tor Hidden Service
+tor_config = """
+HiddenServiceDir /var/lib/tor/Domain/
+HiddenServicePort 80 127.0.0.1:8080
+HiddenServicePort 80 127.0.0.1:8070
+SocksPort 127.0.0.1:9150
+"""
+with open("/etc/tor/torrc", "a") as torrc:
+    torrc.write(tor_config)
+
 time.sleep(5)
-os.system("exit")
+#subprocess.run(["sudo", "systemctl", "restart", "tor"])
+#subprocess.run(["sudo", "cp", "/var/lib/tor/Domain/hostname", "."])
+#subprocess.run(["sudo", "cp", "-r", "/etc/tor", "."])
+#subprocess.run(["sudo", "cp", "-r", "/var/lib/tor", "."])
+#os.rename("tor", "TorDomainInfo")
+
+# Download figlet font "Reg.flf"
+subprocess.run(["wget", "-O", "~/.local/share/fonts/figlet-fonts/Reg.flf", "https://raw.githubusercontent.com/xero/figlet-fonts/master/ANSI%20Regular.flf"])
+
+# Display success message
+print("Installation completed!")
+os.system("clear")
+os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Restart? | lolcat")
+print("Operating instructions : ")
+
+print("=" * 40)
+print("You must restart your device to start the services properly. Do you want to restart your device now? (y/n)")
+print('->>> Run "python3 Run-Server.py" to start service.')
+print("=" * 40)
+x = input(" >>> ")
+if x == "y" or "Y":
+    os.system("reboot")
+else:
+    print("You must restart your device to start the services properly. If you previously restarted your device after installing the program, you can continue.")
+time.sleep(5)
