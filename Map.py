4a0a501a8b2ea610bc63e8975e7bdae793ee3b06
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-27 19:59:30 +0400

    Update Map.py

--- Map.py
+++ Map.py
@@ -2,89 +2,82 @@ import os
 import re
 import time
 import requests
-import plotly.graph_objects as go
- 
+import folium
+import json
+from art import *
+from geopy.geocoders import Nominatim
 
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf IP Map | lolcat")
+# Function to clear the terminal screen based on the operating system
+def clear_screen():
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # Non-Windows (e.g., Linux, macOS)
+        os.system('clear')
 
-# Replace with your preferred geocoding service (e.g., GeoIP2, ipinfo.io)
-GEOCODING_SERVICE_URL = "https://ipinfo.io"
-HTML_FILE_NAME = "ip_globe.html"
-UPDATE_INTERVAL_SECONDS = 5  # Update the globe every 5 minutes
+# Directory where the text files are located (replace with your directory path)
+directory_path = "."
 
-def delete_existing_html_file():
-    if os.path.exists(HTML_FILE_NAME):
-        os.remove(HTML_FILE_NAME)
-
+# Function to extract IP addresses from a text file
 def extract_ip_addresses(file_path):
     ip_addresses = []
     with open(file_path, 'r', encoding='latin-1') as file:
         content = file.read()
+        # Use a regular expression to find IP addresses in the file
         ip_pattern = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
         ip_addresses.extend(re.findall(ip_pattern, content))
     return ip_addresses
 
-def get_location(ip):
-    response = requests.get(f"{GEOCODING_SERVICE_URL}/{ip}/json")
-    if response.status_code == 200:
-        data = response.json()
-        if 'loc' in data:
-            lat, lon = data['loc'].split(',')
-            return float(lat), float(lon)
-    return None
+# Function to create a map with IP markers using Folium
+def create_ip_map(ip_list):
+    m = folium.Map(location=[0, 0], zoom_start=2)  # Initial map with a global view
 
-def create_ip_globe(ip_list):
-    fig = go.Figure()
+    geolocator = Nominatim(user_agent="ip-geolocator")
 
-    # Add a scatter plot of IP addresses
-    for ip_info in ip_list:
-        location = get_location(ip_info)
+    for ip in ip_list:
+        location = get_location(ip, geolocator)
         if location:
-            lat, lon = location
-            fig.add_trace(
-                go.Scattergeo(
-                    lon=[lon],
-                    lat=[lat],
-                    hovertext=[f"IP: {ip_info}"],
-                    mode="markers",
-                    marker=dict(size=8, color="red"),
-                )
-            )
+            folium.Marker(
+                location=location,
+                popup=f"IP: {ip}",
+            ).add_to(m)
 
-    # Customize the layout of the globe
-    fig.update_geos(
-        projection_type="orthographic",
-        showland=True,
-        landcolor="rgb(243, 243, 243)",
-        countrycolor="rgb(204, 204, 204)",
-    )
+    return m
 
-    # Set the layout of the figure
-    fig.update_layout(
-        title="IP Addresses on the Globe",
-        geo=dict(
-            showcoastlines=True,
-        ),
-    )
+# Function to get location data for an IP address using geopy (Nominatim)
+def get_location(ip, geolocator):
+    try:
+        location = geolocator.geocode(ip, timeout=10)  # Increase timeout to 10 seconds
+        if location:
+            return [location.latitude, location.longitude]
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
 
-    # Save the figure as an HTML file
-    fig.write_html(HTML_FILE_NAME)
-
-# Delete the existing HTML file (if any)
-delete_existing_html_file()
-
+# Main loop to continuously monitor and update the IP map
 while True:
+    clear_screen()
+    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf IP Map | lolcat")
+
+
+    # Initialize a list to store extracted IP addresses
     extracted_ip_addresses = []
 
-    for filename in os.listdir():
-        if os.path.isfile(filename):
-            extracted_ip_addresses.extend(extract_ip_addresses(filename))
+    # Process each file in the directory
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if os.path.isfile(file_path):
+            extracted_ip_addresses.extend(extract_ip_addresses(file_path))
 
-    create_ip_globe(extracted_ip_addresses)
+    # Create a map with IP markers
+    ip_map = create_ip_map(extracted_ip_addresses)
 
-    print()
-    print(f"Globe updated and saved as '{HTML_FILE_NAME}'. Waiting for the next update...")
-    os.system("clear")
-    os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf IP Map | lolcat")
-    time.sleep(UPDATE_INTERVAL_SECONDS)
+    # Save the map to an HTML file (you can view it in a web browser)
+    os.system("rm -rf ip_location_map.html")  # Remove existing file
+    ip_map.save("ip_location_map.html")
+
+    print("Map saved as 'ip_location_map.html'")
+    print("Loading >>>")
+
+    # Clear the terminal after each search
+    time.sleep(5)  # Sleep for 2 seconds
+
