b8a0e4f206bf88a262891300acfa1d1d76ee28bb
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-12-15 18:44:52 +0400

    Update Networkreports.py

--- Networkreports.py
+++ Networkreports.py
@@ -8,6 +8,7 @@ def clear_screen():
     else:  
         os.system('clear')
         os.system("figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Network | lolcat")
+
 directory_path = "."
 
 def extract_and_organize_data(file_path):
@@ -41,12 +42,13 @@ def extract_and_organize_data(file_path):
             else:
                 os.system("")
         except json.JSONDecodeError:
-         os.system("")
+            os.system("")
+
+last_num_files = 0
+
 while True:
     extracted_data = []
     file_details = []
-
-    clear_screen()
 
     for filename in os.listdir(directory_path):
         file_path = os.path.join(directory_path, filename)
@@ -54,15 +56,21 @@ while True:
             extract_and_organize_data(file_path)
 
     num_files = len(file_details)
-    if num_files > 0:
-        print(f"Number of Reports: {num_files}")
-        for i, file_path in enumerate(file_details, start=1):
-            print(f"Report {i}: {file_path}")
-        print("\nTargets Data:")
-        for data_row in extracted_data:
-            print(data_row)
-    else:
-        print("---------------------------------------------------------------------------------------")
-        print("No Reports with the specified data structure found.")
-    
+
+    if num_files != last_num_files:
+        clear_screen()
+
+        if num_files > 0:
+            print(f"Number of Reports: {num_files}")
+            for i, file_path in enumerate(file_details, start=1):
+                print(f"Report {i}: {file_path}")
+            print("\nTargets Data:")
+            for data_row in extracted_data:
+                print(data_row)
+        else:
+            print("---------------------------------------------------------------------------------------")
+            print("No Reports with the specified data structure found.")
+
+        last_num_files = num_files
+
     time.sleep(1)
